a0cd70d5bed6790bd1fe56d94c38c9dd6acfd0ea
Andrei Hutusoru <andrei.hutusoru@softvision.ro>
2015-01-09 16:25:18 +0200

    Add test for entering a wrong pin number

--- marketplacetests/payment/app.py
+++ marketplacetests/payment/app.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from random import randint
+
 from gaiatest.apps.base import Base
 from marionette.by import By
 from marionette.wait import Wait
@@ -18,6 +20,7 @@ class Payment(Base):
     _pin_digit_holder_locator = (By.CSS_SELECTOR, '.pinbox span')
     _pin_continue_button_locator = (By.CSS_SELECTOR, '.cta')
     _pin_heading_locator = (By.CSS_SELECTOR, 'section.content h1')
+    _wrong_pin_message_locator = (By.CSS_SELECTOR, '.err-msg')
 
     # Final buy app panel
     _app_name_locator = (By.CSS_SELECTOR, '.product .title')
@@ -33,6 +36,11 @@ class Payment(Base):
         self.wait_for_element_present(*self._payment_frame_locator)
         payment_iframe = self.marionette.find_element(*self._payment_frame_locator)
         self.marionette.switch_to_frame(payment_iframe)
+
+    def generate_random_pin(self, i):
+        range_start = 10**(i-1)
+        range_end = (10**i)-1
+        return randint(range_start, range_end)
 
     @property
     def app_name(self):
@@ -56,8 +64,15 @@ class Payment(Base):
         self.marionette.find_element(*self._pin_container_locator).send_keys(pin)
         self.tap_pin_continue()
 
+    def enter_pin(self, pin):
+        self.wait_for_element_displayed(*self._pin_container_locator)
+        Wait(marionette=self.marionette).until(lambda m: 'Enter PIN' in self.pin_heading)
+        self.marionette.find_element(*self._pin_container_locator).send_keys(pin)
+        self.tap_pin_continue()
+
     def tap_pin_continue(self):
         button = self.marionette.find_element(*self._pin_continue_button_locator)
+        self.wait_for_element_displayed(*self._pin_continue_button_locator)
         Wait(marionette=self.marionette).until(lambda m: button.is_enabled())
         button.tap()
 
@@ -67,9 +82,23 @@ class Payment(Base):
     def tap_buy_button(self):
         self.marionette.switch_to_frame()
         self.wait_for_element_not_displayed(*self._loading_throbber_locator)
-        payment_iframe = self.marionette.find_element(*self._payment_frame_locator)
-        self.marionette.switch_to_frame(payment_iframe)
+        self.switch_to_payment_frame()
         self.marionette.find_element(*self._buy_button_locator).tap()
         self.marionette.switch_to_frame()
         self.wait_for_element_not_present(*self._payment_frame_locator)
         self.marionette.switch_to_frame()
+
+    def tap_cancel_button(self):
+        self.marionette.switch_to_frame()
+        self.wait_for_element_not_displayed(*self._loading_throbber_locator)
+        self.switch_to_payment_frame()
+        cancel_button = self.marionette.find_element(*self._cancel_button_locator)
+        cancel_button.tap()
+        self.marionette.switch_to_frame()
+        self.wait_for_element_not_present(*self._payment_frame_locator)
+        self.marionette.switch_to_frame()
+
+    @property
+    def wrong_pin_message_text(self):
+        self.wait_for_element_displayed(*self._wrong_pin_message_locator)
+        return self.marionette.find_element(*self._wrong_pin_message_locator).text
---
+++ marketplacetests/tests/test_marketplace_enter_incorrect_pin.py
@@ -0,0 +1,46 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from fxapom.fxapom import FxATestAccount
+
+from marketplacetests.payment.app import Payment
+from marketplacetests.marketplace_gaia_test import MarketplaceGaiaTestCase
+from marketplacetests.marketplace.app import Marketplace
+
+
+class TestMarketplaceLoginDuringPurchase(MarketplaceGaiaTestCase):
+
+    def test_enter_incorrect_pin(self):
+
+        APP_NAME = 'Test Zippy With Me'
+        PIN = '1234'
+        acct = FxATestAccount(use_prod=False).create_account()
+
+        if self.apps.is_app_installed(APP_NAME):
+            self.apps.uninstall(APP_NAME)
+
+        marketplace = Marketplace(self.marionette, self.MARKETPLACE_DEV_NAME)
+        marketplace.launch()
+
+        marketplace.set_region('United States')
+
+        details_page = marketplace.navigate_to_app(APP_NAME)
+        ff_accounts = details_page.tap_purchase_button(is_logged_in=False)
+        ff_accounts.login(acct.email, acct.password)
+
+        payment = Payment(self.marionette)
+        payment.create_pin(PIN)
+        payment.tap_cancel_button()
+
+        marketplace.switch_to_marketplace_frame()
+        marketplace.wait_for_notification_message_not_displayed()
+
+        details_page.tap_purchase_button()
+
+        # Enter random wrong pin number
+        incorrect_pin = payment.generate_random_pin(4)
+        payment.enter_pin(incorrect_pin)
+
+        # Verify error message
+        self.assertEqual('Wrong PIN', payment.wrong_pin_message_text)
